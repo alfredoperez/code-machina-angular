e8c97867c04d5c31398a7a5a53aa365d57b04a30
Alfredo <alfredo.perez@sailpoint.com>
2022-06-08 14:08:05 -0500

    initial commit

---
+++ apps/site/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alacarta-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'site';
+}
