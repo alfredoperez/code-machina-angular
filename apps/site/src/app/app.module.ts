a05bfe011f60c5d2349f98baf75fe04b39fd86c5
Alfredo Perez <alfredo.perez.q@gmail.com>
2022-06-18 14:35:04 -0500

    adds navbar

--- apps/site/src/app/app.module.ts
+++ apps/site/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
 import { ModusModule } from "ng-modus";
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, ModusModule],
+    imports: [BrowserModule, ModusModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
